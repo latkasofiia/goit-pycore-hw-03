d714d387fc75993147189ef5f57cbf321264fe76
latkasofiia <latkasofia5@gmail.com>
2025-10-15 15:33:20 +0300

    Task2

--- Task_2.py
+++ Task_2.py
@@ -1,37 +1,33 @@
-# Імпортуємо модуль random для генерації випадкових чисел 
+# Імпортуємо модуль random для генерації випадкових чисел
 import random
 
 # Створюємо функцію і задаємо їй відповідні параметри
-def get_numbers_ticket():
+def get_numbers_ticket(min_num, max_num, quantity):
     
-    # Просимо користувача ввести мінімальне число для лотереї
-    min_number = int(input("Введіть мінімальне число (>=1): "))
-    # Просимо користувача ввести максимальне число для лотереї
-    max_number = int(input("Введіть максимальне число (<=1000): "))
-    # Просимо користувача ввести кількість чисел на квитку 
-    quantity = int(input("Введіть кількість чисел для квитка: "))
-
-    # Перевірка чи параметри правильні 
-    if min_number < 1 or max_number > 1000 or min_number > max_number:
-        print("Некоректні значення мінімуму i максимуму!")
-        return
-    elif quantity < 1 or quantity > (max_number - min_number + 1):
-        print("Некоректна кількість чисел для квитка!")
-        return
+    # Перевірка коректності параметрів
+    if min_num < 1 or max_num > 1000 or min_num > max_num:
+        return []
+    if quantity < 1 or quantity > (max_num - min_num + 1):
+        return []
 
     # Генерація унікальних випадкових чисел
-    ticket = random.sample(range(min_number, max_number + 1), quantity)
+    ticket = random.sample(range(min_num, max_num + 1), quantity)
 
-    # Сортуємо числа 
+    # Сортуємо числа
     ticket.sort()
 
-    # Виводимо результат 
+
+# Виводимо результат
     print("Ваш лотерейний квиток:", ticket)
     print("Мінімальне число:", ticket[0])
     print("Максимальне число:", ticket[-1])
     print("Кількість чисел:", len(ticket))
 
-get_numbers_ticket()
+    return ticket
 
+# Просимо користувача ввести параметри
+min_number = int(input("Введіть мінімальне число (>=1): "))
+max_number = int(input("Введіть максимальне число (<=1000): "))
+quantity = int(input("Введіть кількість чисел для квитка: "))
 
-
+get_numbers_ticket(min_number, max_number, quantity)
